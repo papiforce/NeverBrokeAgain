f45f04c60f83fad10b509e05334c14ca43896769
Emmanuel Kasomo <59349327+papiforce@users.noreply.github.com>
2020-08-26 23:26:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const cfg = require('./index.json');
+const prefix = ("!");
+
+bot.on('ready', function() {
+    console.log("Je suis prêt!");
+    bot.user.setActivity("Greed Island").catch(console.error);
+});
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send("Bienvenue sur le serveur " + member.displayName + "!");
+        console.log(`${member.displayName} à rejoins le serveur.`);
+    }).catch(console.error)
+});
+
+bot.on('message', msg => {
+    if(msg.content === prefix + "site") {
+      msg.channel.send("https://www.emmanuel-kasomo.yo.fr");
+      console.log("Une personne a demandé le lien de ton site!");
+    }
+});
+
+bot.login(cfg.token);
